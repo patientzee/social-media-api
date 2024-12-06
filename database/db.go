6275a13ec6e06211236fbb60e39c7faf524f700b
keagilwe Zungu <keagilwezungu@hotmail.com>
2024-12-06 08:54:32 +0200

    updated db.go file

--- database/db.go
+++ database/db.go
@@ -1,1 +1,20 @@
+package database
 
+import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+// InitDatabase connects to the database.
+func InitDatabase() {
+	var err error
+	DB, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+	log.Println("Connected to database")
+}
